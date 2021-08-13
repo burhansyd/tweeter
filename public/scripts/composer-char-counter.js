b89bacc08a6ba6fb60a3ff6801b658ff65306756
Burhan Syed <burhan.syed@outlook.com>
2021-08-13 20:45:30 +0000

    amend char counter to use text val length instead of incrementing manually

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,36 +1,18 @@
-let charsLeft = 140;
-
-const increaseCharCount = () => {
-  charsLeft += 1;
-  return charsLeft;
-};
-
-const decreaseCharCount = () => {
-  charsLeft -= 1;
-  return charsLeft;
-}
-
 const overLimit = () => charsLeft < 0;
 
 $(document).ready(function () {
-  $('#tweet-text').on('keydown', function(event) {
-    if (event.keyCode === 8) {
-      if (charsLeft < 140) {
-        increaseCharCount();
-      }
-    } else {
-      decreaseCharCount();
-    }
-
+  $('#tweet-text').on('input', function(event) {
+    const maxCharCount = 140;
+    const charCount = $(this).val().length;
     const counter = $(this).closest(".new-tweet").find(".counter");
 
-    if (overLimit()) {
+    if (charCount > maxCharCount) {
       $(counter).addClass('negative-count');
     } else if ($(counter).hasClass('negative-count')) {
       $(counter).removeClass('negative-count');
     }
 
-    $(counter).val(charsLeft);
+    $(counter).val(maxCharCount - charCount);
 
   });
 });
